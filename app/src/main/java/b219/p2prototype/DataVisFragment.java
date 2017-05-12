a7e9434261013f90c2f66c93bec83965dd52b051
tgechev <trendafil_gechev@abv.bg>
2017-05-12 17:43:02 +0200

    Update DataVisFragment.java
    line 25 method isEmpty() replaced with !=null because of possible exception

--- app/src/main/java/b219/p2prototype/DataVisFragment.java
+++ app/src/main/java/b219/p2prototype/DataVisFragment.java
@@ -22,7 +22,7 @@ public class DataVisFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView;
-        if(getArguments().getParcelableArrayList(USER_INPUT).isEmpty()){
+        if(getArguments().getParcelableArrayList(USER_INPUT)!=null){
             rootView = inflater.inflate(R.layout.fragment_no_data, container, false);
         }
         else {
